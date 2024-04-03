f307e0a8f4869a532052d691fc09a0d2328e0a8e
Thomas Bellembois <thomas.bellembois@uca.fr>
2024-04-03 18:45:09 +0200

    Generate JSON file.

---
+++ src/main.rs
@@ -0,0 +1,79 @@
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+    time::{SystemTime, UNIX_EPOCH},
+};
+
+use log::debug;
+use rusqlite::Connection;
+use serde::Serialize;
+
+#[derive(Serialize, Debug)]
+struct Realm<'a> {
+    realm: &'a str,
+    users: Vec<Person<'a>>,
+}
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Debug)]
+struct Person<'a> {
+    id: i32,
+    username: String,
+    email: String,
+    emailVerified: bool,
+    createdTimestamp: u128,
+    enabled: bool,
+    totp: bool,
+    credentials: Vec<String>,
+    disableableCredentialTypes: Vec<String>,
+    requiredActions: Vec<String>,
+    realmRoles: Vec<&'a str>,
+    notBefore: u8,
+    groups: Vec<String>,
+}
+
+fn main() {
+    env_logger::init();
+
+    let current_system_time = SystemTime::now();
+    let duration_since_epoch = current_system_time.duration_since(UNIX_EPOCH).unwrap();
+    let milliseconds_timestamp = duration_since_epoch.as_millis();
+
+    let connection = Connection::open("storage.db").unwrap();
+
+    let mut stmt = connection
+        .prepare("SELECT person_id, person_email FROM person")
+        .unwrap();
+    let people: Vec<Person> = stmt
+        .query_map([], |row| {
+            Ok(Person {
+                id: row.get(0)?,
+                username: row.get(1)?,
+                email: row.get(1)?,
+                emailVerified: true,
+                createdTimestamp: milliseconds_timestamp,
+                enabled: true,
+                totp: true,
+                credentials: Vec::new(),
+                disableableCredentialTypes: Vec::new(),
+                requiredActions: Vec::new(),
+                realmRoles: ["default-roles-chimitheque"].to_vec(),
+                notBefore: 0,
+                groups: Vec::new(),
+            })
+        })
+        .unwrap()
+        .map(|p| p.unwrap())
+        .collect();
+
+    let realm = Realm {
+        realm: "chimitheque",
+        users: people,
+    };
+    debug!("{:?}", realm);
+
+    let file = File::create("keycloak.json").unwrap();
+    let mut writer = BufWriter::new(file);
+    serde_json::to_writer(&mut writer, &realm).unwrap();
+    writer.flush().unwrap();
+}
